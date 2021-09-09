857ecf99d728fbccf45a2b0fa27b2a34454d7ec8
username1103 <username1103@naver.com>
2021-09-09 22:45:55 +0900

    [Add] test validation

--- src/validations/auth.validation.ts
+++ src/validations/auth.validation.ts
@@ -2,8 +2,14 @@ import Joi from 'joi';
 import { validationSchema } from './type';
 
 export const register: validationSchema = {
-  body: Joi.object().keys({
+  body: Joi.object({
     phone: Joi.string().required().example('01050568216').description('phone number'),
     verifCode: Joi.string().required().example('777777').description('verification code'),
+  }),
+};
+
+export const logout: validationSchema = {
+  params: Joi.object({
+    user: Joi.string().required().example('213123').description('ggggdsdsds'),
   }),
 };
