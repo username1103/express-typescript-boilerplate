174642eee306d9f3d55df73bd4f6d51255a38dcc
username1103 <username1103@naver.com>
2021-09-12 23:14:43 +0900

    [Add] token, user schema

---
+++ src/entity/Token.ts
@@ -0,0 +1,26 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, ManyToOne } from 'typeorm';
+import { User } from './User';
+
+@Entity()
+export class Token {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  token: string;
+
+  @ManyToOne((type) => User, (user) => user.tokens)
+  user: User;
+
+  @Column()
+  expires: Date;
+
+  @Column()
+  blacklisted: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,13 +1,23 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, OneToMany } from 'typeorm';
+import { Token } from './Token';
 
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn()
-  id?: number;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-  @Column()
+  @Column({ unique: true })
   phone: string;
 
   @Column({ nullable: true })
   name: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @OneToMany((type) => Token, (token) => token.id)
+  tokens: Token[];
 }
