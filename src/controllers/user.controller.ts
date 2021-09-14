141b0fd63ea70b36743c481063ccb31e1af31b35
username1103 <username1103@naver.com>
2021-09-14 22:06:44 +0900

    [Fix] getUser contoller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,11 +1,18 @@
+import httpStatus from 'http-status';
 import { User } from '../entities/User';
 import { GetUserRequest } from '../interfaces/validations';
+import ApiError from '../utils/ApiError';
 import catchAsync from '../utils/catchAsync';
+import { errorDatas } from '../utils/errorData';
 
 export const getUser = catchAsync(async (req, res) => {
   const { userId } = req.params as GetUserRequest['params'];
 
   const user = await User.findOne(userId);
+
+  if (user === undefined) {
+    throw new ApiError(httpStatus.NOT_FOUND, errorDatas.USER_NOT_FOUND);
+  }
 
   res.send(user);
 });
